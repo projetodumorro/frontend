7f2eadd3fb6338eb3bfa2d9bdcd586d3ec227d23
Micael Dos Santos <micael.augustons@hotmail.com>
2023-07-27 14:29:09 -0300

    Task-8 - Implementando Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Home from './pages/home/Home';
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  const [valor, setValor] = useState(0);
+
+  function handleClick() {
+    setValor(valor + 1);
+  }
+  return (
+    //  <Home  title="Componente Home"
+    //         description="Este é um componente Home que recebe props."/>
+    <div>
+      <p>O valor é: {valor}</p>
+      <button onClick={handleClick}>Adicionar 1</button>
+    </div>
+  );
+}
+
+export default App;
