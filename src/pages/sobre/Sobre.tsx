2f84533ec5d8329997533f154642a0a8e55f5af8
Isamiguel <goncalvesisabellamiguel@gmail.com>
2023-08-11 10:05:46 -0300

    Sobre

--- src/pages/sobre/Sobre.tsx
+++ src/pages/sobre/Sobre.tsx
@@ -6,18 +6,19 @@ import {
     Avatar,
     Card,
     Typography,
-  } from "@material-tailwind/react"
+} from "@material-tailwind/react"
 
 function Sobre() {
     return (
 <>
 <div className="bg-gray-100 min-h-screen p-10">
       <div className="max-w-3xl mx-auto bg-white p-6 rounded-lg shadow">
-        <h1 className="text-3xl font-bold mb-4 text-orange-500">Quem Somos Nós</h1>
-        <p className="text-gray-700">
-          Somos uma organização comprometida em fortalecer favelas e comunidades, reconhecendo suas culturas únicas
-          e impulsionando suas economias locais. Através do poder do empreendedorismo e da criatividade, estamos
-          construindo um futuro onde cada voz é ouvida e cada talento é valorizado.
+        <h1 className="text-3xl font-bold mb-4 text-orange-500">DUMORRO</h1>
+        <p className="text-gray-500">
+         
+
+
+
         </p>
         <p className="text-gray-700 mt-4">
           Nossa equipe diversificada trabalha incansavelmente para criar oportunidades que inspirem mudanças positivas
@@ -53,7 +54,6 @@ function Sobre() {
             </Typography>
           </div>
         </ListItem>
-        <ListItem>
           <ListItemPrefix>
             <Avatar variant="circular" alt="emma" src="https://media.licdn.com/dms/image/D4D35AQEfZlvjfaN7JA/profile-framedphoto-shrink_800_800/0/1689050297731?e=1692122400&v=beta&t=eohjpjjhS4wt1iHOu9fMdgYmH8snj-e9jIU_TO2ysyE" />
           </ListItemPrefix>
@@ -65,14 +65,14 @@ function Sobre() {
               UI/UX Designer @ Material Tailwind
             </Typography>
           </div>
-        </ListItem>
+          
+
       </List>
     </Card>
 
     </div>
-
-    
-    </>
+  
+  </>
     );
     
 }
