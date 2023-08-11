c7f93b1d4b10ea01e39f9af556b12ce28294c745
projetodumorro <projetodumorro@gmail.com>
2023-08-11 16:14:41 -0300

    Sobre Estilos

--- src/pages/sobre/Sobre.tsx
+++ src/pages/sobre/Sobre.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
     List,
     ListItem,
@@ -5,18 +6,19 @@ import {
     Avatar,
     Card,
     Typography,
-  } from "@material-tailwind/react"
+} from "@material-tailwind/react"
 
 function Sobre() {
     return (
 <>
 <div className="bg-gray-100 min-h-screen p-10">
       <div className="max-w-3xl mx-auto bg-white p-6 rounded-lg shadow">
-        <h1 className="text-3xl font-bold mb-4 text-orange-500">Quem Somos Nós</h1>
-        <p className="text-gray-700">
-          Somos uma organização comprometida em fortalecer favelas e comunidades, reconhecendo suas culturas únicas
-          e impulsionando suas economias locais. Através do poder do empreendedorismo e da criatividade, estamos
-          construindo um futuro onde cada voz é ouvida e cada talento é valorizado.
+        <h1 className="text-3xl font-bold mb-4 text-orange-500">DUMORRO</h1>
+        <p className="text-gray-500">
+         
+
+
+
         </p>
         <p className="text-gray-700 mt-4">
           Nossa equipe diversificada trabalha incansavelmente para criar oportunidades que inspirem mudanças positivas
@@ -52,7 +54,6 @@ function Sobre() {
             </Typography>
           </div>
         </ListItem>
-        <ListItem>
           <ListItemPrefix>
             <Avatar variant="circular" alt="emma" src="https://media.licdn.com/dms/image/D4D35AQEfZlvjfaN7JA/profile-framedphoto-shrink_800_800/0/1689050297731?e=1692122400&v=beta&t=eohjpjjhS4wt1iHOu9fMdgYmH8snj-e9jIU_TO2ysyE" />
           </ListItemPrefix>
@@ -64,12 +65,36 @@ function Sobre() {
               UI/UX Designer @ Material Tailwind
             </Typography>
           </div>
-        </ListItem>
+          <div>
+            <Typography variant="h6" color="blue-gray">
+              Felipe Vitorino
+            </Typography>
+            <Typography variant="small" color="gray" className="font-normal">
+              UI/UX Designer @ Material Tailwind
+            </Typography>
+          </div>
+          <div>
+            <Typography variant="h6" color="blue-gray">
+              Felipe Vitorino
+            </Typography>
+            <Typography variant="small" color="gray" className="font-normal">
+              UI/UX Designer @ Material Tailwind
+            </Typography>
+          </div>
+          <div>
+            <Typography variant="h6" color="blue-gray">
+              Felipe Vitorino
+            </Typography>
+            <Typography variant="small" color="gray" className="font-normal">
+              UI/UX Designer @ Material Tailwind
+            </Typography>
+          </div>
       </List>
     </Card>
 
     </div>
-    </>
+  
+  </>
     );
     
 }
